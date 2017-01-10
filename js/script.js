c36d3af35ebfaf7d040424ddfe7141bc95df434c
市川ひなた <hinakami0701@gmail.com>
2017-01-10 15:59:48 +0900

    固定ヘッダーの途中です

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-$(function(){
-	
-});
+window.onload = function(){
+	var headerfixedbox = $(".header-fixed-box").offset().top;
+
+	$(window).scroll(function () {
+
+		if ($(window).scrollTop() > headerfixedbox){
+			$(".header-fixed-box").css({"position": "fixed", "top": "0px", "background-image": "url(images/sky.png)"});
+		}else{
+			$(".header-fixed-box").css({"position": "relative","top": "90vh","background-image": "none"});
+		}
+	})
+}
