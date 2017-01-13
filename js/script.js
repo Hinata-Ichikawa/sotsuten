5b0d8291e9b604f3ca471a0583a62bd716387572
松本 和馬 <hello@mtmtkzm.com>
2017-01-13 16:35:19 +0900

    update header opacity animation

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 $(function(){
 	var headerfixedbox = $(".header-fixed-box").offset().top;
-
+	var $fixedTarget = $('.header-fixed-box');
 	$(window).scroll(function () {
 
-		if ($(window).scrollTop() > headerfixedbox){
-			$(".header-fixed-box").addClass("fixed");
-			$(".header-layer").animate({
-				"opacity": "1"
-			},500);
-		}else{
-			$(".header-fixed-box").removeClass("fixed");
-			// $(".header-layer").animate({
-			// 	"opacity": "0"
-			// },500);
+		if ($(window).scrollTop() > headerfixedbox) {　// スクロール位置が下の方だったら
+			if (!$fixedTarget.hasClass('fixed')) {　// ヘッダーが固定されてなかったら
+				$(".header-fixed-box").addClass("fixed");
+				$(".header-layer").animate({
+					"opacity": "1"
+				}, 300);
+			}
+		} else { // スクロール位置が上の方だったら
+			if ($fixedTarget.hasClass('fixed')) { // ヘッダーが固定されていたら
+				$(".header-fixed-box").removeClass("fixed");
+				$(".header-layer").animate({
+					"opacity": "0"
+				}, 300);
+			}
 		}
 	})
 
