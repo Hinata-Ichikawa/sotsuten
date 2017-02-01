59829f1734040ee58b7171d062326988095992ea
松本 和馬 <hello@mtmtkzm.com>
2017-02-01 21:41:12 +0900

    各所微調整

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(function(){
 	var headerfixedbox = $(".header-fixed-box").offset().top;
 	var $fixedTarget = $('.header-fixed-box');
-	$(window).scroll(function () {
+	$(window).on('scroll resize load', function () {
 		if ($(window).scrollTop() >= headerfixedbox) {　// スクロール位置が下の方だったら
 			if (!$fixedTarget.hasClass('fixed')) {　// ヘッダーが固定されてなかったら
 				$(".header-fixed-box").addClass("fixed");
