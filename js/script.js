c547d7305bbe1ed5e0a7721394c24644eb19ff42
松本 和馬 <hello@mtmtkzm.com>
2017-02-05 01:40:18 +0900

    update change jquery to javascript

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
 $(function(){
-	var headerfixedbox = $(".header-fixed-box").offset().top;
+	var headerfixedbox = document.querySelector('.header-fixed-box').offsetTop;
 	var $fixedTarget = $('.header-fixed-box');
+
 	$(window).on('scroll resize load', function () {
-		if ($(window).scrollTop() >= headerfixedbox) {　// スクロール位置が下の方だったら
-			if (!$fixedTarget.hasClass('fixed')) {　// ヘッダーが固定されてなかったら
-				$(".header-fixed-box").addClass("fixed");
-				$(".header-layer").animate({
-					"opacity": "1"
+		if ($(window).scrollTop() >= headerfixedbox) {
+			if (!$fixedTarget.hasClass('fixed')){
+				$('.header-fixed-box').addClass('fixed');
+				$('.header-layer').animate({
+					'opacity': '1'
 				}, 300);
 			}
-		} else { // スクロール位置が上の方だったら
-			if ($fixedTarget.hasClass('fixed')) { // ヘッダーが固定されていたら
-				$(".header-fixed-box").removeClass("fixed");
-				$(".header-layer").animate({
-					"opacity": "0"
+		} else {
+			if ($fixedTarget.hasClass('fixed')) {
+				$('.header-fixed-box').removeClass('fixed');
+				$('.header-layer').animate({
+					'opacity': '0'
 				}, 300);
 			}
 		}
-	})
+	});
 
 	// スムーススクロール
 	$('a[href^="#"]').on('click', function() {
